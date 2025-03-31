ff83382f8a4c00be92f43d2240db65d2a1ba830a
David Angarita <davidangarita.sistemas@gmail.com>
2025-03-31 18:20:16 -0500

    Delete error classes and import from _gh_service

--- src/github_helper/_api.py
+++ src/github_helper/_api.py
@@ -8,20 +8,9 @@ import logistro
 import orjson
 
 from . import _gh_service as srv
+from ._gh_service import GHError, ScopesError, ScopesWarning
 
 _logger = logistro.getLogger(__name__)
-
-
-class GHError(RuntimeError):
-    """Error type for `gh` CLI tool errors."""
-
-
-class ScopesError(RuntimeError):
-    """Error for when missing necessary scope."""
-
-
-class ScopesWarning(UserWarning):
-    """Warning for when missing optional enhancing scope."""
 
 
 class GHApi:
