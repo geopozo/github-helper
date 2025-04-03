1aacc4d126e8c7aaba480496caa83a710a74619b
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-03 15:16:22 -0500

    Create transform user data method

--- src/github_helper/_gh_adapter.py
+++ src/github_helper/_gh_adapter.py
@@ -4,3 +4,8 @@ class GHAdapter:
     def __init__(self, json, pretty):
         self._json = json
         self._pretty = pretty
+
+    def transform_user_data(self, user_data):
+        if self._json or self._pretty:
+            return {"user": user_data}
+        return [[user_data]]
