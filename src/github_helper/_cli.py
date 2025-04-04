8911cd3db5dd423d24b03d5325a5e0bac6f5e438
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-04 16:04:41 -0500

    Change flag

--- src/github_helper/_cli.py
+++ src/github_helper/_cli.py
@@ -72,7 +72,7 @@ def _get_cli_args():
         help="Return all repos of current logged in user.",
     )
     repos_parser.add_argument(
-        "-pg",
+        "-p",
         "--paginate",
         help="Get all repos",
         action="store_true",
