34422890375d483246270099f44e8d4408496981
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-04 15:17:21 -0500

    Create _audit functions

---
+++ src/github_helper/api/_audit.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import jsondiff as jd
+
+from github_helper._gh_service import GHError
+from github_helper._utils import load_json
+
+_SCRIPT_DIR = Path(__file__).resolve().parent
+_TEMPLATE_PATH = _SCRIPT_DIR / "templates"
+
+
+def _validate_config_keys(keys):
+    valid_keys = {"repo", "include", "exclude"}
+    if keys - valid_keys:
+        raise TypeError(f"Only keys {valid_keys} are allowed.")
+
+
+def get_required_rulesets(configs, repo_full_name):
+    rulesets_files = set()
+    for cfg in configs:
+        _validate_config_keys(cfg.keys())
+
+        if "repo" not in cfg:
+            continue
+        if "include" in cfg:
+            if not isinstance(cfg["include"], list):
+                raise TypeError("'include' must be a list")
+            if cfg["repo"] == "*" or cfg["repo"] == repo_full_name:
+                rulesets_files = rulesets_files | set(cfg["include"])
+        if "exclude" in cfg:
+            if not isinstance(cfg["exclude"], list):
+                raise TypeError("'exclude' must be a list")
+            if cfg["repo"] == repo_full_name:
+                rulesets_files = rulesets_files - set(cfg["exclude"])
+    return rulesets_files
+
+
+async def load_template_ruleset(path):
+    if Path(path).is_file():
+        return await load_json(path=path)
+
+    if Path(path).is_dir():
+        raise GHError("File name required")
+
+    template_path = _TEMPLATE_PATH / path
+    return await load_json(path=template_path)
+
+
+def remove_excluded_keys(ruleset, excluded_keys):
+    for key in excluded_keys:
+        if key in ruleset:
+            ruleset.pop(key)
+
+
+async def json_diff(original, target, diffs):
+    json_diffs = jd.diff(original, target, marshal=True)
+    if not json_diffs:
+        return []
+    else:
+        for diff_key in json_diffs:
+            if diff_key == "$delete":
+                for i in json_diffs[diff_key]:
+                    diffs.append({"status": f"additional key: {i}"})
+            else:
+                diffs.append({"status": diff_key})
+    return diffs
