cbcd6863b68be482bc96023f507f3f81cdb6fb4e
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-04 17:24:49 -0500

    Change adapter logic for orgs

--- src/github_helper/_gh_adapter.py
+++ src/github_helper/_gh_adapter.py
@@ -23,9 +23,11 @@ class GHAdapter:
         self._pretty = pretty
 
     def transform_orgs_data(self, orgs_data):
-        if self._json or self._pretty:
-            return orgs_data
-        return [[org["name"], org["role"]] for org in orgs_data]
+        if self._json:
+            return self._to_json_string(orgs_data)
+        for org in orgs_data:
+            org["name"] = org["name"][:24]
+        return self._to_table(orgs_data)
 
     def transform_user_data(self, user_data):
         if self._json or self._pretty:
