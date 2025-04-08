429423c16e924d051a58b3fa63e0b73d5bbaecfc
Andrew Pikul <ajpikul@gmail.com>
2025-04-08 18:48:46 -0500

    Add styles and more columns

--- src/github_helper/_api_to_html_adapter.py
+++ src/github_helper/_api_to_html_adapter.py
@@ -7,6 +7,40 @@ from htmy import Component, Context, Renderer, component, html
 _logger = logistro.getLogger(__name__)
 
 github_com = r"https://www.github.com"
+style = """
+span.topic {
+  border: 1px solid black;
+  padding: 0rem .5rem;
+  border-radius: 10px;
+  margin: 0 .1rem;
+}
+td.owner {
+  text-align: right;
+}
+td.repo {
+  max-width: 15rem;
+}
+td.description {
+  max-width: 50rem;
+}
+tr.repo-row td a:link,
+tr.repo-row td a:hover,
+tr.repo-row td a:visited,
+tr.repo-row td a:focus,
+tr.repo-row td a:active {
+  color: black;
+}
+body {
+  overflow-x: auto;
+  width: 100%;
+}
+table {
+  width: max-content;
+}
+tr.repo-row:nth-child(even) {
+  background-color: #f0f0f0;
+}
+"""
 
 
 @dataclass(frozen=True, kw_only=True, slots=True)
@@ -17,15 +51,43 @@ class RepoRow:
         repo = self.repo
         _logger.debug(f"Building html row for {repo['name']}")
         return html.tr(
+            html.td(html.span("📌" if repo["pinned"] else "")),
             html.td(
+                html.a(repo["owner"], href=f"{github_com}/{repo['owner']}"),
+                class_="owner",
+            ),
+            html.td(html.span("/")),
+            html.td(
+                html.a(
+                    repo["name"],
+                    href=f"{github_com}/{repo['owner']}/{repo['name']}",
+                ),
+                class_="repo",
+            ),
+            html.td(html.span("⑂" if repo["fork"] else "")),
+            html.td(
+                html.span(repo["description"] or ""),
+                class_="description",
+            ),
+            html.td(
+                html.a(
+                    "🔧",
+                    href=f"{github_com}/{repo['owner']}/{repo['name']}/settings/access",
+                ),
                 *[
-                    html.a(repo["owner"], href=f"{github_com}/{repo['owner']}"),
-                    html.span("/"),
-                    html.a(
-                        repo["name"],
-                        href=f"{github_com}/{repo['owner']}/{repo['name']}",
-                    ),
+                    html.a(s, href=f"{github_com}/{s}") if s != "(404)" else s
+                    for s in repo["collaborators"]
                 ],
+                class_="collaborators",
+            ),
+            html.td(
+                *[html.span(s, class_=f"topic {s}") for s in repo["topics"]],
+                class_="topics",
+            ),
+            class_=(
+                "repo-row "
+                f"{repo['visibility']} "
+                f"{'archived' if repo['archived'] else ''}"
             ),
         )
 
@@ -42,6 +104,9 @@ async def repos(repos_data):
     page = (
         html.DOCTYPE.html,
         html.html(
+            html.head(
+                html.style(style),
+            ),
             html.body(
                 table,
             ),
