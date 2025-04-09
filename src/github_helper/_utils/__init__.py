9a7d7d4dfc193c5f578e9e5fed6d0feb15fe530e
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-09 16:39:50 -0500

    Create ErrorSerializer and AutoEncoder classes

--- src/github_helper/_utils/__init__.py
+++ src/github_helper/_utils/__init__.py
@@ -1,5 +1,6 @@
 """A util functions."""
 
+import json
 from pathlib import Path
 
 import aiofiles
@@ -13,3 +14,15 @@ async def load_json(path):
     async with aiofiles.open(path) as f:
         file = await f.read()
     return orjson.loads(file)
+
+
+class ErrorSerializer:
+    def __json__(self):
+        return f"Error: {self!s}"
+
+
+class AutoEncoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, "__json__"):
+            return o.__json__()  # manual call here
+        return super().default(o)
