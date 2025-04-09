0cb17703b61b74dc34fce77add5c265ab1b99989
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-09 16:38:30 -0500

    Change king of Error

--- src/github_helper/_utils/__init__.py
+++ src/github_helper/_utils/__init__.py
@@ -5,12 +5,10 @@ from pathlib import Path
 import aiofiles
 import orjson
 
-from github_helper._gh_service import GHError
-
 
 async def load_json(path):
     if not Path(path).is_file():
-        raise GHError(f"{path} not exist")
+        raise FileNotFoundError(f"{path} not exist")
 
     async with aiofiles.open(path) as f:
         file = await f.read()
