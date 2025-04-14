8dac11faae76e58e8c706f31f43ae539abc84cec
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-14 18:48:47 -0500

    Create to_table in a new file

---
+++ src/github_helper/_adapters/to_table.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+
+
+def to_table(data, *, pretty=False, headers="keys"):
+    return tabulate(
+        data,
+        headers=headers if pretty else "",
+        tablefmt="psql" if pretty else "plain",
+    )
