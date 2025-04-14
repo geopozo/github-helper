e071a38ca990b4c903ba74d84463a89c91ec8a92
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-14 16:31:38 -0500

    Create init for _adapters

---
+++ src/github_helper/_adapters/__init__.py
@@ -0,0 +1,5 @@
+"""GitHub Helper is a tool for checking the various statuses of your repositories."""
+
+from .api_to_cli import GHAdapter
+
+__all__ = ["GHAdapter"]
--- src/github_helper/_cli.py
+++ src/github_helper/_cli.py
@@ -6,7 +6,7 @@ import sys
 
 import logistro
 
-from github_helper._api_to_cli_adapter import GHAdapter
+from github_helper._adapters import GHAdapter
 
 from . import api
 
