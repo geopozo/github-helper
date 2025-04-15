b387118998e023dc355bc765b36a3d52d70f5d23
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-15 18:04:30 -0500

    Rename to_json function

--- src/github_helper/_adapters/to_json.py
+++ src/github_helper/_adapters/to_json.py
@@ -3,7 +3,7 @@ import json
 from github_helper._utils import AutoEncoder
 
 
-def to_json_string(data, *, pretty=False):
+def to_string(data, *, pretty=False):
     return json.dumps(
         data,
         indent=2 if pretty else 0,
