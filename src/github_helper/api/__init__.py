06726e64e516d95894d3b1a014638f63871942d2
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-16 18:57:21 -0500

    Create logic for audit versions

--- src/github_helper/api/__init__.py
+++ src/github_helper/api/__init__.py
@@ -13,7 +13,7 @@ from github_helper._services import gh as srv
 from github_helper._services.gh import GHError, ScopesError, ScopesWarning
 from github_helper._utils import load_json
 from github_helper.api import _audit
-from github_helper.api._compare_versions import order_versions
+from github_helper.api._compare_versions import filter_versions, order_versions
 
 _logger = logistro.getLogger(__name__)
 _SCRIPT_DIR = Path(__file__).resolve().parent
@@ -344,4 +344,17 @@ class GHApi:
             "repo" and owner is assumed to be the current user.
 
         """
-        return repo, 0
+        tags, sadness = await self.get_tagged_versions(repo)
+        releases, sadness = await self.get_releases(repo)
+        filtered_tags = filter_versions(tags, "version")
+        filtered_releases = filter_versions(releases, "tag")
+        versions = filtered_tags | filtered_releases
+        result = [
+            {
+                "version": v,
+                "tags": v in filtered_tags,
+                "releases": v in filtered_releases,
+            }
+            for v in versions
+        ]
+        return result, sadness
