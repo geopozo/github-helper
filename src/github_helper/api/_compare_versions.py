b2ce8c061f201f5beadaeec2626b6141a265b8bb
Andrew Pikul <ajpikul@gmail.com>
2025-04-18 20:42:51 -0500

    Add draft structure for release audit.

--- src/github_helper/api/_compare_versions.py
+++ src/github_helper/api/_compare_versions.py
@@ -1,6 +1,55 @@
 import re
+from dataclasses import field
+from typing import NamedTuple
 
 from packaging import version
+
+GlibcReference = {
+    "2.17": "Most compatible.",
+    "2.28": "Debian 10. Reasonably Compatible >2018",
+    "2.29": "Transitional Version.",
+    "2.31": "Ubuntu 20.04",
+    "2.34": "Ubuntu 22.04",
+    "2.35": "Ubuntu >22.04.1. 2022+",
+    "2.36": "New?",
+    "2.38": "Released Yesterday?",
+}
+GlibcRecommended = ("2.17", "2.28", "2.31", "2.34")
+
+
+class GlibcVersions(dict):
+    """Simple wrapper of dict."""
+
+
+class ArchSet(NamedTuple):
+    x86: bool | GlibcVersions | None
+    x86_64: bool | GlibcVersions | None
+    arm64: bool | GlibcVersions | None
+    arm32: bool | GlibcVersions | None
+
+
+class ProjectAudit:
+    name: str
+    language: str
+
+
+class PyProjectAudit(ProjectAudit):
+    pure_python: bool
+    compiled: bool
+    minimum_interpreter: str
+    abi3_all: bool
+    inconsistent_options: bool
+    win: ArchSet
+    mac: ArchSet
+    linux_glibc: ArchSet
+    linux_musl: ArchSet
+
+    non_compliant: field(default_factory=list[str])
+
+
+class ReleaseAudit:
+    prerelease_agree: bool
+    projects: field(default_factory=list[ProjectAudit])
 
 
 def order_versions(versions: list[dict], key: str):
