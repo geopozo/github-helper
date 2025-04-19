426083363368538e42cd44298dd69a29ea89c60d
Andrew Pikul <ajpikul@gmail.com>
2025-04-19 13:50:03 -0500

    Add logic to process filenames

--- src/github_helper/api/_compare_versions.py
+++ src/github_helper/api/_compare_versions.py
@@ -1,77 +1,268 @@
+"""
+Github and Pypi versions can contain multiple files: here are tools.
+
+a) the versions can have multiple projects (separated by name).
+
+b) compiled binary extensions: When writing python (or javascript) extensions
+in C, C++, rust, etc, you have to create a separate binary for each
+environment you wish to support, and there are several levels.
+
+Here is a list of the environments we check for:
+
+.
+|-- Windows
+|   |-- x86_64
+|   |-- Arm64
+|   `-- Win32
+|-- Mac
+|   |-- Apple ARM
+|   |-- Intel
+|   `-- Universal (1 binary twice the size, 2 architectures)
+`-- Linux
+    |-- x86_64
+    |   |-- Glibc-2.17
+    |   |-- Glibc-2.28
+    |   |-- Glibc-2.31
+    |   |-- Glibc-2.35
+    |   `-- musl
+    |-- Arm64
+    |   |-- Glibc-2.17
+    |   |-- Glibc-2.28
+    |   |-- Glibc-2.31
+    |   |-- Glibc-2.35
+    |   `-- musl
+    `-- Arm32
+        |-- Glibc-2.17
+        |-- Glibc-2.28
+        |-- Glibc-2.31
+        |-- Glibc-2.35
+        `-- musl
+
+On top of that, we have to see what is the minimum python version supported for
+each one.
+"""
+
 import re
-from dataclasses import field
+from collections.abc import MutableMapping
+from dataclasses import dataclass, field
+from fnmatch import fnmatch
 from typing import NamedTuple
 
+import logistro
 import semver
-from packaging import version
+from packaging import utils, version
+from packaging.version import Version as V  # noqa: N817 I like my V()
+
+_logger = logistro.getLogger(__name__)
+
 
 GlibcReference = {
-    "2.17": "Most compatible.",
-    "2.28": "Debian 10. Reasonably Compatible >2018",
-    "2.29": "Transitional Version.",
-    "2.31": "Ubuntu 20.04",
-    "2.34": "Ubuntu 22.04",
-    "2.35": "Ubuntu >22.04.1. 2022+",
-    "2.36": "New?",
-    "2.38": "Released Yesterday?",
+    V(2.17): "Most compatible.",
+    V(2.28): "Debian 10. Reasonably Compatible >2018",
+    V(2.29): "Transitional Version.",
+    V(2.31): "Ubuntu 20.04",
+    V(2.34): "Ubuntu 22.04",
+    V(2.35): "Ubuntu >22.04.1. 2022+",
+    V(2.36): "New?",
+    V(2.38): "Released Yesterday?",
 }
-GlibcRecommended = ("2.17", "2.28", "2.31", "2.34")
+"""
+A list of common Glibc versions and what theyare as of 2025-04-19.
+
+What is Glibc? It is GNU's libc, and libc is the most fundamental dependency
+of C and C++, it offers most of the operating system's API. Compiled programs
+can be compiled with a lower version for compatibility, or a higher version for
+speed and features. However, compiling against a higher version will make it
+unsupported on older distros where everything is compiled against a lower
+version.
+"""
+
+GlibcRecommended = (V(2.17), V(2.28), V(2.31), V(2.34))
+"""List of libc versions recommended as of 2025-04-19."""
 
 
-class GlibcVersions(dict):
-    """Simple wrapper of dict."""
+class CompatibilityMatrix(NamedTuple):
+    """
+    Compiled python extensions (eg. in c or rust) minimum supported pythons.
+
+    When compiling python extensions, version restrictions come from both
+    a) headers and b) ABI (Application Binary Interface, the cousin of API). As
+    for a) headers, you can only use a version of python >= the version of the
+    headers you used. As for b) ABI, If you do not compile with abi3, it means
+    you MUST run the extension against the very specific interpreter you
+    compiled against.
+    """
+
+    abi3: bool
+    python_h: V
 
 
-class ArchSet(NamedTuple):
-    x86: bool | GlibcVersions | None
-    x86_64: bool | GlibcVersions | None
-    arm64: bool | GlibcVersions | None
-    arm32: bool | GlibcVersions | None
+class GlibcMatrix(dict[V, CompatibilityMatrix]):
+    """Describes python compatibility for each Glibc you compiled against."""
+
+
+@dataclass
+class ArchSet:
+    x86: CompatibilityMatrix | GlibcMatrix | None = None
+    x86_64: CompatibilityMatrix | GlibcMatrix | None = None
+    universal: CompatibilityMatrix | None
+    arm64: CompatibilityMatrix | GlibcMatrix | None = None
+    arm32: CompatibilityMatrix | GlibcMatrix | None = None
+
+    @classmethod
+    def for_glibc(cls):
+        return cls(
+            x86=GlibcMatrix(),
+            x86_64=GlibcMatrix(),
+            arm64=GlibcMatrix(),
+            arm32=GlibcMatrix(),
+        )
 
 
 class ProjectAudit:
     name: str
-    language: str
-    sdist: bool
+    files: field(default_factory=list[str])
+    languages: field(default_factory=set[str])
+
+    def __init__(self, name):
+        self.name = name
+        self.files = []
 
 
 class PyProjectAudit(ProjectAudit):
-    pure_python: bool
-    compiled: bool
-    minimum_interpreter: str
-    abi3_all: bool
-    inconsistent_options: bool
+    pure_python: bool | None = None
+    with_sdist: bool = False
+    with_bdist: bool = False
+    is_compiled: bool = False
     win: ArchSet
     mac: ArchSet
     linux_glibc: ArchSet
     linux_musl: ArchSet
 
-    non_compliant: field(default_factory=list[str])
+    def __init__(self, name, filename, rest):
+        super().__init__(name)
+        self.pure_python = None
+        self.with_sdist = False
+        self.with_bdist = False
+        self.is_compiled = False
+        self.win = ArchSet()
+        self.mac = ArchSet()
+        self.linux_glibc = ArchSet.for_glibc()
+        self.linux_musl = ArchSet()
+        self.add_file(filename, rest)
+
+    def add_file(self, filename, rest):  # noqa: PLR0912, C901 complexity
+        if filename.endswidth("tar.gz"):
+            self.with_sdist = True
+            return True
+        elif filename.endswidth(".whl"):
+            (
+                version,
+                build_tag,
+                python_tag,
+                abi_tags,
+                platform_tags,
+            ) = *rest
+            _logger.debug(f"whl attributes: {rest}")
+            self.with_bdist = True
+            if python_tag in ("py3", "py2.py3"):
+                self.pure_python = True
+                self.files.append(filename)
+                return True
+            else:
+                self.pure_python = False
+                self.is_compiled = True
+                match = re.match(r"cp3(\d+)", python_tag)
+                if not match:
+                    return False
+                py_h = V(f"3.{match.group(1)}")
+
+                if abi_tags != "abi3":
+                    return False
+                cm = CompatibilityMatrix(abi3=True, python_h=py_h)
+                match platform_tags:
+                    case "win_amd64":
+                        self.win.x86_64 = cm
+                    case "win32":
+                        self.win.x86 = cm
+                    case "win_arm64":
+                        self.win.arm64 = cm
+                    case p if fnmatch(p, "macosx_*_x86_64"):
+                        self.mac.x86_64 = cm
+                    case p if fnmatch(p, "macosx_*_arm64"):
+                        self.mac.arm64 = cm
+                    case p if fnmatch(p, "macosx_*_universal2"):
+                        self.mac.universal = cm
+                    case p if (m := re.match(r"manylinux(2014|_\d_\d)_(.*)^", p)):
+                        if m.group(1) == "2014":
+                            v = V("2.17")
+                        else:  # process_group
+                            v = V(m.group(1)[1:].replace("_", "."))
+                        match m.group(2):
+                            case "x86_64":
+                                self.linux_glibc.x86_64[v] = cm
+                            case "aarch64":
+                                self.linux_glibc.amd64[v] = cm
+                            case "armv71l":
+                                self.linux_glibc.amd32[v] = cm
+                    case p if (m := re.match(r"musllinux_(\d_\d)_(.*)^", p)):
+                        match m.group(2):
+                            case "x86_64":
+                                self.linux_musl.x86_64 = cm
+                            case "aarch64":
+                                self.linux_musl.x86 = cm
+                            case "armv7l":
+                                self.linux_musl.amd64 = cm
+        return False
+
+        # identify operating system
+
+
+class Projects:
+    projects: field(default_factory=MutableMapping[str, ProjectAudit])
+    unknown_files: field(default_factory=list[str])
+    incompliant_files: field(default_factory=list[str])
+
+    def add_file(self, filename):
+        if filename.endswidth("tar.gz") or filename.endswith(".whl"):
+            try:
+                name, *rest = utils.parse_sdist_filename(filename)
+                if name not in self.projects:
+                    self.projects[name] = PyProjectAudit(name, filename, rest)
+                elif not self.projects[name].add_file(filename, rest):
+                    self.incompliant_files.append(filename)
+            except utils.InvalidSdistFilename:
+                self.unknown_files.append(filename)
+                return
+        elif filename.endswidth(".whl"):
+            try:
+                name, *rest = utils.parse_wheel_filename(filename)
+                if name not in self.projects:
+                    self.projects[name] = PyProjectAudit(name, filename, rest)
+                elif not self.projects[name].add_file(filename, rest):
+                    self.incompliant_files.append(filename)
+            except utils.InvalidWheelFilename:
+                self.incompliant_files.append(filename)
+                return
+        else:
+            self.unknown_files.append(filename)
+            return
 
 
 class ReleaseAudit:
     prerelease_agree: bool
-    projects: field(default_factory=list[ProjectAudit])
+    projects: Projects
 
     def __init__(self, release):
         self.version = explode_versions(release["tag"])
         if not self.version:
             return
         self.prerelease_agree = self.version["is_prerelease"] == release["prerelease"]
-        self.projects = {}
-        # for file in release["files"]
-        # create projects
-        # find wheels, find bdists
-        # minimum interpreter version (c-constraint)
-        # universal abi3
-        # supports windows
-        # amd64
-        # arm64
-        # x86
-        # supports mac
-        # silicon
-        # intel
-        # universal
+        self.unknown = []
+        self.incompliant = []
+        self.projects = Projects()
+        for file in release["files"]:
+            self.projects.add_file(file)
 
     def __str__(self):
         if not self.prerelease_agree:
