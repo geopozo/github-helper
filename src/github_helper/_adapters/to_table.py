3ed7fe69299394c2fbe3b65243a83eab94c0fe81
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-21 16:47:55 -0500

    Add col align to tabulate

--- src/github_helper/_adapters/to_table.py
+++ src/github_helper/_adapters/to_table.py
@@ -1,9 +1,10 @@
 from tabulate import tabulate
 
 
-def format_table(data, *, pretty=False, headers="keys"):
+def format_table(data, *, pretty=False, headers="keys", colalign=None):
     return tabulate(
         data,
         headers=headers if pretty else "",
         tablefmt="psql" if pretty else "plain",
+        colalign=colalign,
     )
