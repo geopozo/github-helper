6ed2aee2918dd8e70cd82c0cea265db9dfd93eff
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-21 15:14:35 -0500

    Add logger to AutoEncoder

--- src/github_helper/_utils/__init__.py
+++ src/github_helper/_utils/__init__.py
@@ -4,7 +4,10 @@ import json
 from pathlib import Path
 
 import aiofiles
+import logistro
 import orjson
+
+_logger = logistro.getLogger(__name__)
 
 
 async def load_json(path):
@@ -23,6 +26,7 @@ class ErrorSerializer:
 
 class AutoEncoder(json.JSONEncoder):
     def default(self, o):
+        _logger.debug2(o)
         if hasattr(o, "__json__"):
             return o.__json__()  # manual call here
         return super().default(o)
