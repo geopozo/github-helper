63a1d47fede5ac497f9312df850b79ddf898c43a
David Angarita <davidangarita.sistemas@gmail.com>
2025-04-24 17:31:34 -0500

    Remove scripts and import from load_file

--- src/github_helper/_adapters/to_html/__init__.py
+++ src/github_helper/_adapters/to_html/__init__.py
@@ -140,69 +140,7 @@ async def repos(repos_data):
     _logger.debug("Building page.")
     scripts = [
         html.script(src="https://cdn.tailwindcss.com"),
-        html.script(
-            html.SafeStr("""
-    function updateVisibleRowClasses() {
-        const rows = [...document.querySelectorAll('table tbody tr')];
-        let visibleIndex = 0;
-
-        rows.forEach(row => {
-          row.classList.remove('odd', 'even');
-
-          if (row.style.display !== 'none') {
-            row.classList.add(visibleIndex % 2 === 0 ? 'even' : 'odd');
-            visibleIndex++;
-          }
-    });
-  }"""),
-        ),
-        html.script(
-            html.SafeStr(r"""
-  const publicCheckbox = document.getElementById('toggle-public');
-  const privateCheckbox = document.getElementById('toggle-private');
-  const archivedCheckbox = document.getElementById('toggle-archive');
-  const ownerInput = document.getElementById('owner-filter');
-  const repoInput = document.getElementById('repo-filter');
-  const modal = document.getElementById("my-modal");
-
-  const openModal = (data) => {
-    const iframe = modal.querySelector("iframe");
-    modal.style.display = "grid";
-    iframe.src = data;
-  }
-
-  const closeModal = () => modal.style.display = "none";
-
-  function filterAll() {
-    console.log("Filtering All.")
-    const ownerFilter = ownerInput.value.toLowerCase();
-    const repoFilter = repoInput.value.toLowerCase();
-    const rows = document.querySelectorAll('table tbody tr');
-
-    rows.forEach(row => {
-      const matchOwner = [...row.querySelectorAll('td.owner')].some(td =>
-        td.textContent.toLowerCase().includes(ownerFilter)
-      );
-      const matchRepo = [...row.querySelectorAll('td.repo')].some(td =>
-        td.textContent.toLowerCase().includes(repoFilter)
-      );
-      archived = !(!archivedCheckbox.checked && row.classList.contains('archived'))
-      private  = !(!privateCheckbox.checked && row.classList.contains('private'))
-      public   = !(!publicCheckbox.checked && row.classList.contains('public'))
-
-      toDisplay = matchOwner && matchRepo && archived && private && public
-      row.style.display = toDisplay ? '' : 'none';
-    });
-    updateVisibleRowClasses();
-  }
-  publicCheckbox.addEventListener('change', filterAll);
-  privateCheckbox.addEventListener('change', filterAll);
-  archivedCheckbox.addEventListener('change', filterAll);
-  ownerInput.addEventListener('input', filterAll);
-  repoInput.addEventListener('input', filterAll);
-  filterAll();
-  """),
-        ),
+        html.script(html.SafeStr(html.SafeStr(await load_file(_JS_PATH / "repos.js")))),
     ]
     page = (
         html.DOCTYPE.html,
