77940e4758fa902d38bbe59b29057f70b966fa28
Andrew Pikul <ajpikul@gmail.com>
2025-04-27 23:45:45 -0500

    Increase default count to 20

--- src/github_helper/api/__init__.py
+++ src/github_helper/api/__init__.py
@@ -468,7 +468,7 @@ class GHApi:
     async def audit_versions(  # noqa: C901
         self,
         repo: str,
-        count: int = 10,
+        count: int = 20,
     ) -> RetVal[list[dict]]:
         """
         Verify that version of a repository have differences.
