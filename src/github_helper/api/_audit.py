db374414da36ac88e97e552eed29693f8af83778
Andrew Pikul <ajpikul@gmail.com>
2025-05-02 18:29:52 -0500

    Improve remove_excluded_keys

--- src/github_helper/api/_audit.py
+++ src/github_helper/api/_audit.py
@@ -49,7 +49,9 @@ async def load_template_ruleset(path):
     return await load_json(path=template_path)
 
 
-def remove_excluded_keys(ruleset, excluded_keys):
-    for key in excluded_keys:
-        if key in ruleset:
-            ruleset.pop(key)
+def remove_key(d, dotted_keys):
+    for dotted_key in dotted_keys:
+        keys = dotted_key.split(".")
+        for key in keys[:-1]:
+            d = d.get(key, {})
+        d.pop(keys[-1], None)
