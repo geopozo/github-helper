b1a17aff272d5663d84a3a8844e7141148f2f58f
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-05 13:53:00 -0500

    Add experimental component for select filter

--- src/github_helper/_adapters/to_html/_components.py
+++ src/github_helper/_adapters/to_html/_components.py
@@ -32,6 +32,67 @@ def modal(
     )
 
 
+# experimental component
+def select_filter(title: str, options: list) -> Component:
+    return html.div(
+        html.details(
+            html.summary(
+                html.span(title, class_="text-sm font-medium"),
+                html.span("🔽", class_="transition-transform group-open:-rotate-180"),
+                class_=(
+                    "flex items-center gap-2 border-b border-gray-300 "
+                    "pb-1 text-gray-700 transition-colors hover:border-gray-400 "
+                    "hover:text-gray-900 [&::-webkit-details-marker]:hidden "
+                    "cursor-pointer"
+                ),
+            ),
+            html.div(
+                html.div(
+                    html.button(
+                        "Reset",
+                        type_="button",
+                        class_=(
+                            "text-sm text-gray-700 underline "
+                            "transition-colors hover:text-gray-900"
+                        ),
+                    ),
+                    class_="flex items-center justify-between px-3 py-2",
+                ),
+                html.fieldset(
+                    html.legend("Checkboxes", class_="sr-only"),
+                    html.div(
+                        *[
+                            html.label(
+                                html.input_(
+                                    type_="checkbox",
+                                    class_="size-5 rounded border-gray-300 shadow-sm",
+                                ),
+                                html.span(
+                                    o.label,
+                                    class_="text-sm font-medium text-gray-700",
+                                ),
+                                for_="Option1",
+                                class_="inline-flex items-center gap-3",
+                            )
+                            for o in options
+                        ],
+                        class_="flex flex-col items-start gap-3",
+                    ),
+                    class_="p-3",
+                ),
+                class_=(
+                    "z-auto w-64 divide-y divide-gray-300 "
+                    "rounded border border-gray-300 "
+                    "bg-white shadow-sm group-open:absolute "
+                    "group-open:start-0 group-open:top-8"
+                ),
+            ),
+            class_="group relative",
+        ),
+        class_="flex gap-4 sm:gap-6",
+    )
+
+
 async def render_page(
     heads: Iterable[core.BaseTag],
     content: Iterable[core.BaseTag],
