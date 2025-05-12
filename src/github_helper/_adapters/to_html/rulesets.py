03f88d8741421b155f06ad298a4209065db6b63c
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-12 14:36:58 -0500

    Create RulesetRow class

--- src/github_helper/_adapters/to_html/rulesets.py
+++ src/github_helper/_adapters/to_html/rulesets.py
@@ -1,7 +1,9 @@
+from collections.abc import MutableMapping
+from dataclasses import dataclass
 from pathlib import Path
 
 import logistro
-from htmy import html
+from htmy import Component, Context, component, html
 
 from github_helper._adapters.to_html import _components
 from github_helper._utils import load_file
@@ -12,9 +14,27 @@ _HTML_DIR = Path(__file__).resolve().parent
 _STYLES_PATH = _HTML_DIR / "_styles"
 
 
-async def rulesets_template():
+@dataclass(frozen=True, kw_only=True, slots=True)
+class RulesetRow:
+    ruleset: MutableMapping
+
+    def _error_printer(self, s):
+        return html.span(str(type(s).__name__), title=str(s))
+
+    async def htmy(self, context: Context) -> Component:  # noqa: ARG002
+        ruleset = self.ruleset
+        return html.tr(html.td(ruleset.get("status")))
+
+
+@component
+def rulesets_rows(rulesets, context: Context) -> Component:  # noqa: ARG001
+    return [RulesetRow(ruleset=r) for r in rulesets]
+
+
+async def rulesets_template(rulesets_data):
     _logger.debug("Building table.")
     styles = html.style(await load_file(_STYLES_PATH / "rulesets.css"))
+    table = html.table(rulesets_rows(rulesets_data), class_="mx-auto")
     _logger.debug("Building page.")
-    content = []
+    content = [table]
     return await _components.render_page([styles], content)
