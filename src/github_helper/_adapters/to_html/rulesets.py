c5303eed1f59ef4209f7984551db37e37e4734a3
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-12 10:01:40 -0500

    Create base html for rulesets

---
+++ src/github_helper/_adapters/to_html/rulesets.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import logistro
+from htmy import html
+
+from github_helper._adapters.to_html import _components
+from github_helper._utils import load_file
+
+_logger = logistro.getLogger(__name__)
+
+_HTML_DIR = Path(__file__).resolve().parent
+_STYLES_PATH = _HTML_DIR / "_styles"
+
+
+async def rulesets():
+    _logger.debug("Building table.")
+    styles = html.style(await load_file(_STYLES_PATH / "rulesets.css"))
+    _logger.debug("Building page.")
+    content = []
+    return await _components.render_page([styles], content)
