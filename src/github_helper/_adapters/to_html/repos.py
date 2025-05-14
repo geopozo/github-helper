5b2b601f1a8af5716c1e48af2b084d67ded907d9
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-14 10:39:02 -0500

    Add bg color to search bar

--- src/github_helper/_adapters/to_html/repos.py
+++ src/github_helper/_adapters/to_html/repos.py
@@ -223,7 +223,7 @@ async def repos_template(repos_data):
                 class_="mx-auto",
                 id_="controls",
             ),
-            class_="sticky top-0 w-full p-2 z-10",
+            class_="sticky top-0 w-full bg-white p-2 z-10",
         ),
         table,
         modal_iframe,
